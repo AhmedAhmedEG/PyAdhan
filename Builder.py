4db863bcba13d1792465c21c63cb3c9e37a59b78
AhmedAhmedEG <ahmed.ahmed.a.m.aa12@gmail.com>
2025-06-02 19:23:18 +0300

    PyAdhan v2.10.10.7:-
    [Bug Fixes]
    -Fixed adhan API endpoint to use https as now it's enforced and not optional as before.

--- Builder.py
+++ Builder.py
@@ -1,28 +1,47 @@
+import shutil
+from pathlib import Path
+
 from cx_Freeze import setup, Executable
 from Modules.Config import Config
 import os
 
 config = Config()
 
-file_excludes = ['Qt6WebEngineCore.dll', 'lupdate.exe', 'Qt6Designer.dll', 'Qt6Quick.dll', 'Qt6DesignerComponents.dll',
-                 'qtwebengine_devtools_resources.pak', 'qtwebengine_resources.pak', 'Qt6Qml.dll',
-                 'Qt6QuickTemplates2.dll', 'Qt6Quick3DRuntimeRender.dll']
+file_excludes = [
+    'Qt6WebEngineCore.dll', 'lupdate.exe', 'Qt6Designer.dll',
+    'Qt6Quick.dll', 'Qt6DesignerComponents.dll', 'qtwebengine_devtools_resources.pak',
+    'qtwebengine_resources.pak', 'Qt6Qml.dll', 'Qt6QuickTemplates2.dll',
+    'Qt6Quick3DRuntimeRender.dll'
+]
 
-module_excludes = ['tkinter', 'PySide6.qml', 'PySide6.translations.qtwebengine_locales', 'PySide6.QtWebEngineCore',
-                   'PySide6.QtNetwork', 'PySide6.QtNfc', 'PySide6.QtWebChannel', 'PySide6.QtWebEngine', 'PySide6.QtSql',
-                   'PySide6.QtBluetooth', 'PySide6.QtWebEngineWidgets', 'PySide6.QtWebKit', 'PySide6.QtScript',
-                   'PySide6.QtWebKitWidgets', 'PySide6.QtWebSockets']
+module_excludes = [
+    'tkinter', 'PySide6.qml', 'PySide6.translations.qtwebengine_locales',
+    'PySide6.QtWebEngineCore', 'PySide6.QtNetwork', 'PySide6.QtNfc',
+    'PySide6.QtWebChannel', 'PySide6.QtWebEngine', 'PySide6.QtSql',
+    'PySide6.QtBluetooth', 'PySide6.QtWebEngineWidgets', 'PySide6.QtWebKit',
+    'PySide6.QtScript', 'PySide6.QtWebKitWidgets', 'PySide6.QtWebSockets'
+]
 
-setup(name=f'PyAdhan',
-      description='PyAdhan',
-      version=config.APP_VERSION,
-      options={'build_exe': {'include_files': [('Resources', 'Resources'),
-                                               ('ChangeLog.txt', 'ChangeLog.txt')],
-                             'excludes': module_excludes,
-                             'build_exe': f'build/PyAdhan v{config.APP_VERSION}'}},
+app_name = f'PyAdhan v{config.APP_VERSION}'
 
-      executables=[Executable(script='PyAdhan.py', icon='Resources/Icon.ico', base='Win32GUI')])
-
+setup(
+    name=app_name,
+    description='PyAdhan',
+    version=config.APP_VERSION,
+    options={
+        'build_exe': {
+            'include_files': [
+                ('Resources', 'Resources'),
+                ('ChangeLog.txt', 'ChangeLog.txt')
+            ],
+            'excludes': module_excludes,
+            'build_exe': f'build/PyAdhan v{config.APP_VERSION}'
+        }
+    },
+    executables=[
+        Executable(script='PyAdhan.py', icon='Resources/Icon.ico', base='Win32GUI')
+    ]
+)
 
 for root, dirs, files in os.walk('build'):
 
@@ -30,3 +49,6 @@ for root, dirs, files in os.walk('build'):
 
         if filename in file_excludes:
             os.remove(os.path.join(root, filename))
+
+output_path = Path(__file__).parent / f'build/{app_name}'
+shutil.make_archive(str(output_path), 'zip', str(output_path))
